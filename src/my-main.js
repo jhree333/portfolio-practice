c20cb21220c761a49f37b07dda91224fa94a2530
jhree333 <jhree333@gmail.com>
2023-08-16 12:56:22 +0900

    feat: js home 스크롤시 콘텐츠 투명하게 작성

--- src/my-main.js
+++ src/my-main.js
@@ -1,10 +1,18 @@
+// header의 페이지 스크롤시 이벤트
 const header = document.querySelector('.header');
 const headerHeight = header.clientHeight;
-
 window.addEventListener('scroll', () => {
   if (window.scrollY > headerHeight) {
     header.classList.add('header--scrolled');
   } else {
     header.classList.remove('header--scrolled');
   }
+});
+
+// home의 페이지 스크롤시 이벤트
+const home = document.querySelector('.home__contents');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  const opacity = 1 - scrollY / homeHeight;
+  home.style.opacity = opacity;
 });
