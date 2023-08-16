c13ecd990860f161eaa2320c3d4022ed3a125c3d
jhree333 <jhree333@gmail.com>
2023-08-16 16:24:58 +0900

    feat: js projects 필터링 작성

--- src/my-projects.js
+++ src/my-projects.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const filterButton = document.querySelectorAll('.category');
+const projects = document.querySelectorAll('.project');
+
+filterButton.forEach((button) => {
+  button.addEventListener('click', () => {
+    const selectedCategory = button.getAttribute('data-category');
+
+    filterButton.forEach((button) => {
+      button.classList.remove('category--selected');
+    });
+    button.classList.add('category--selected');
+
+    projects.forEach((project) => {
+      const projectCategory = project.getAttribute('data-category');
+
+      if (selectedCategory === 'all' || selectedCategory === projectCategory) {
+        project.style.display = 'block';
+      } else {
+        project.style.display = 'none';
+      }
+    });
+  });
+});
